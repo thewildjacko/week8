0f2454925e359db8db0b6bf0678a26e98e062872
Jake Smolowe <jakesmolowe@gmail.com>
2024-07-26 07:38:43 -0700

    Initial Commit

---
+++ week8/week8App.swift
@@ -0,0 +1,17 @@
+//
+//  week8App.swift
+//  week8
+//
+//  Created by Jake Smolowe on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct week8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
